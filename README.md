db157128b530102bea25a3e6a3549615d1548a76
Areeb Ayubi <sample@madeup.com>
2022-08-06 04:56:37 +0100

    Started to implement the SAC algorithm

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # mscHRL
 Hierarchical Reinforcement Learning using Hierarchical Proximal Policy Optimization (HiPPO) for MSc summer project
+
+
+Plan:
+1. Implement working SAC algorithm
+    a. Make sure that it is compatible with gin
+    b. Make sure that it works in CartPole in OpenAI gym
+2. Build HIDIO
+    a. Integrate working SAC algorithm into HIDIO
+    b. Ensure that relevant functions and classes are gin configurable
+3. Test in authors' environment
--- hidio/sac/utils.py
+++ hidio/sac/utils.py
@@ -1,2 +1,32 @@
 import os
+#import functools
+import gin
+import numpy as np
 from collections import namedtuple, OrderedDict
+import torch.distributions as td
+
+
+
+#def get_invertable(cls):
+#    """"""
+    
+#    class NewCls(cls):
+#        __init__ = functools.partialmethod(cls.__init__, cache_size=1)
+#        
+#    return NewCls
+
+
+@gin.configurable
+def calc_default_target_entropy(spec: TensorSpec, min_prob: float = 0.1):
+    """"""
+    
+    zeros = np.zeros(spec.shape)
+    min_max = np.broadcast(spec.minimum, spec.maximum, zeros)
+    cont = spec.is_continuous
+    log_mp = np.log(min_prob)
+    entropy = np.sum([np.log(M - m) + log_mp 
+                      if cont else min_prob*(np.log(M - m) - log_mp) 
+                      for M,m,_ in min_max])
+    
+    return entropy
+
