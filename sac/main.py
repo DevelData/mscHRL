2e6bbc7bf6234759d199488e26dc2d99830c3604
Areeb Ayubi <sample@madeup.com>
2022-08-09 10:37:54 +0100

    Finished main.py and copies utils.py - untested

---
+++ sac/main.py
@@ -0,0 +1,51 @@
+import pybullet_envs
+import gym
+import numpy as np
+from sac_torch import Agent
+from utils import plot_learning_curve
+
+if __name__ == "__main__":
+    env = gym.make("InvertedPendulumBulletEnv-v0")
+    agent = Agent(input_dims=env.observation_space.shape, 
+                  env=env, 
+                  num_actions=env.action_space.shape[0])
+    n_games = 250
+    filename = "inverted_pendulum.png"
+    figure_file = "./plots/" + filename
+    
+    best_score = env.reward_range[0]
+    score_history = []
+    load_checkpoint = False
+    
+    if load_checkpoint:
+        agent.load_models()
+        env.render(mode="human")
+    
+    for i in range(n_games):
+        observation = env.reset()
+        done = False
+        score = 0
+        
+        while not done:
+            action = agent.choose_action(observation)
+            next_observation, reward, done, info = env.step(action)
+            score += reward
+            agent.remember(observation, action, next_observation, reward, done)
+            
+            if not load_checkpoint:
+                agent.learn()
+            observation = next_observation
+        score_history = score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
+        
+        if avg_score > best_score:
+            best_score = avg_score
+            if not load_checkpoint:
+                agent.save_models()
+        
+        print("Episode = {}, Score = {:.1f}, Average Score = {:.2f}".format(i, score, avg_score))
+        
+    if not load_checkpoint:
+        x = [i for i in range(1, n_games)]
+        plot_learning_curve(x, score_history, figure_file)
+        
